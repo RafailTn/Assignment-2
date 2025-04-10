6074cd8506953cf6feb0e98dfb49706d3278c663
Rafail <rafailadam46@gmail.com>
2025-04-11 00:50:40 +0300

    removed extra file and done preprocessing and started classes

--- src/func_classes.py
+++ src/func_classes.py
@@ -4,18 +4,16 @@ Additional time would be needed to
 write a proper test file
 """
 # Loading the libraries
+from atom import ATOMClassifier
 from sklearn.pipeline import Pipeline
-from sklearn.decomposition import PCA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, fbeta_score
 from sklearn.svm import SVC
 from sklearn.utils import resample
 from pathlib import Path
-from sklearn.model_selection import cross_val_score, RepeatedStratifiedKFold
-import optuna
+from sklearn.model_selection import RepeatedStratifiedKFold
 import os
 import joblib
 import numpy as np
@@ -23,12 +21,10 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 from typing import Optional, List
-from lightgbm import LGBMClassifier
-
 class Utils():
     def __init__(self):
         pass
-    def load_scaler(self):
+    def load_scaler_and_imputer(self):
         """
         Loads the standard scaler from the models directory.
         Returns:
@@ -37,31 +33,39 @@ class Utils():
         # Load the scaler
         cwd = Path.cwd()
         root = cwd.parent
-        scaler_path = root / "models" / "Standardscaler.pkl"
+        scaler_path = root / "models" / "RobustScaler.pkl"
+        imputer_path = root / "models" / "KNNImputer.joblib"
         scaler = joblib.load(scaler_path)
-        return scaler
+        imputer = joblib.load(imputer_path)
+        return scaler, imputer
 
     def create_pipeline(
         self,
         model: object,
         scaler: bool = False,
-        feature_selector: Optional[object] = None
+        feature_selector: Optional[object] = None,
+        imputer: Optional[object] = None,
     ):
         """
         Creates a pipeline for the model.
         Args:
             model: The model object.
             scaler: Whether to use a scaler.
-            feature_selector: The feature selector object.
+            feature_selector: The feature selector object,
+            imputer: The imputer object.
         Returns:
             pipeline: The pipeline object.
         """
         # Initialize the steps for the pipeline
         steps = []
-        # Add the scaler if needed
-        # Load the scaler
-        scaler = self.load_scaler()
-        steps.append(('scaler', scaler))
+        # Add the scaler and imputer if needed
+        if scaler:
+            scaler, imputer = self.load_scaler_and_imputer()
+            steps.append(('imputer', imputer))
+            steps.append(('scaler', scaler))
+        else:
+            steps.append(('imputer', 'passthrough'))
+            steps.append(('scaler', 'passthrough'))
         # Add the feature selector
         if feature_selector is not None:
             steps.append(('feature_selector', feature_selector))
@@ -71,44 +75,53 @@ class Utils():
         steps.append(('model', model))
         pipeline = Pipeline(steps)
         return pipeline
-
-    def train_model_and_predict(
+    
+    # Function to create a boxplot of the scores
+    def create_boxplot(
         self,
-        X_train: pd.DataFrame,
-        y_train: pd.Series,
-        x_test: pd.DataFrame,
-        pipeline: Pipeline,
-        root_path: str = '',
-        filename: str = '',
-        save: bool = False,
-        default_path: bool = True
+        scores_list: List[List[float]],
+        model_name: str,
+        metrics: List[str],
+        means: List[float],
+        stds: List[float],
+        medians: List[float]
     ):
         """
-        Trains the model, stores it and makes predictions.
+        Creates a boxplot of the scores.
         Args:
-            X_train: The training data.
-            y_train: The training labels.
-            x_test: The test data.
-            pipeline: The pipeline object.
-            root_path: Assignment-1 folder
-            filename: Name of the output file or custom path after root if default_path=False
-            save: Whether to save the model
-            default_path: whether to save in models (True) or another folder (False)
+            scores_list: The list of scores.
+            model_name: The name of the model.
+            metrics: The metrics to plot.
         """
-        # Fit the pipeline to the training data
-        pipeline.fit(X_train, y_train)
-        # Save the pipeline, default path is the models folder
-        if save:
-            if default_path:
-                model_path = root_path / 'models' / filename
-                joblib.dump(pipeline, model_path)
-            # Save the pipeline to the specified path, this is for the winner model
-            else:
-                model_path = os.path.join(root_path, filename)
-                joblib.dump(pipeline, model_path)
-        # Make predictions
-        y_pred = pipeline.predict(x_test)
-        return y_pred, pipeline
+        n = len(metrics)
+        fig, axes = plt.subplots(1, n, figsize=(8 * n, 6))
+        # If there's only one metric, ensure axes is iterable.
+        if n == 1:
+            axes = [axes]
+        for i, metric in enumerate(metrics):
+            ax = axes[i]
+            sns.boxplot(data=scores_list[i], color="skyblue", ax=ax)
+            ax.axhline(means[i], color='red', linestyle='dashed', linewidth=2, label=f'Mean: {means[i]:.3f}')
+            ax.axhline(medians[i], color='blue', linestyle='dashed', linewidth=2, label=f'Median: {medians[i]:.3f}')
+            ax.axhline(means[i] - stds[i], color='purple', linestyle='dashdot', linewidth=2, label=f'Mean - 1 Std: {(means[i]- stds[i]):.3f}')
+            ax.axhline(means[i] + stds[i], color='purple', linestyle='dashdot', linewidth=2, label=f'Mean + 1 Std: {(means[i]+ stds[i]):.3f}')
+            ax.set_xlabel("Samples")
+            ax.set_ylabel(f"{metric} Score")
+            ax.set_title(f"{metric} for {model_name}")
+            ax.legend()
+        plt.tight_layout()
+        plt.show()
+
+    def calculate_statistics(scores):
+        """
+        Calculates the mean, standard deviation, and median of a list of scores.
+        Args:
+            scores: The list of scores.
+        """
+        mean = np.mean(scores)
+        std = np.std(scores)
+        median = np.median(scores)
+        return mean, std, median
 
 class rnCV():
     def __init__(self):
@@ -117,16 +130,34 @@ class rnCV():
     def kfold_loop(
         self,
         x: pd.DataFrame,
-        y: pd.Series,
-        pipeline: Pipeline,
+        y: pd.DataFrame,
+        models: List[object],
+        metric: str,
+        metrics: List[str],
         n_splits: int = 3,
-        n_repeats: int = 5,
-        n_outer: int = 10,
-        estimators: List[object] = [LogisticRegression(), RandomForestClassifier(), LGBMClassifier(), SVC(), LinearDiscriminantAnalysis()],
-        hyperparameters: List[dict] = [{'C': [1, 10], 'class_weight': ['balanced']}, {'max_depth': [2, 4, 6, 8, 10], 'n_estimators': [100, 200, 300, 400, 500]}, {'n_estimators': [100, 200, 300, 400, 500], 'max_depth': [2, 4, 6, 8, 10]}, {'C': [1, 10], 'class_weight': ['balanced']}, {'C': [1, 10, 100, 1000], 'class_weight': ['balanced']}],
+        outer_cv: int = 5,
+        n_repeats: int = 10,
+        verbose: bool = False,
     ):
-        pass
+        atom = ATOMClassifier(x, y)
+        tnrs = []
+        fnrs = []
+        tprs = []
+        fprs = []
+        aucs = []
+        mccs = []
 
+        for r in range(n_repeats):
+            atom.run(models, metric=metrics, n_trials=50, cv=RepeatedStratifiedKFold(n_splits=n_splits, n_repeats=outer_cv, random_state=r))
+            fprs.append(atom.results['FPR'])
+            tprs.append(atom.results['TPR'])
+            tnrs.append(atom.results['TNR'])
+            fnrs.append(atom.results['FNR'])
+            aucs.append(atom.results['AUC'])
+            mccs.append(atom.results['MCC'])
+        
+        return fprs, tprs, tnrs, fnrs, aucs, mccs
+        
 def main():
     pass
 
